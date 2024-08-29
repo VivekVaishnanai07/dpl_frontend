894b061460d8666dd11f24bca3c3fb17040b832a
Vivek Vaishnani <91829247+VivekVaishnanai07@users.noreply.github.com>
2024-08-29 16:29:48 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,24 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+// Disable right-click and specific keyboard shortcuts
+document.addEventListener('contextmenu', (e: any) => e.preventDefault());
+
+function ctrlShiftKey(e: any, keyCode: any) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e: any) => {
+  if (
+    e.keyCode === 123 || // F12
+    ctrlShiftKey(e, 'I') || // Ctrl + Shift + I
+    ctrlShiftKey(e, 'J') || // Ctrl + Shift + J
+    ctrlShiftKey(e, 'C') || // Ctrl + Shift + C
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) // Ctrl + U
+  )
+    return false;
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
